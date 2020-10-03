d80b14ab9e8b9658012a770a0ed8a5f44987c56b
Allegra Pusateri <allegrapusateri@gmail.com>
2020-10-02 21:06:57 -0500

    Finalized site footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//////////////////////////////////////////////////////////////////////////////
+import Ampersand from './ampersand.png';
 //////////////////////////////////////////////////////////////////////////////
 import './Footer.css';
 //////////////////////////////////////////////////////////////////////////////
@@ -8,8 +11,21 @@ class Footer extends Component {
     render() {
         return (
             <>
-                <footer className='footer'>
+                <footer>
+                    <div className='overall_container'>
+                        <div className='footer_diagonal'>
+                            <Link to="/" className='link_style' style={{ textDecoration: 'none' }}><p>HOME</p></Link>
+                            <a href='mailto:allegrapusateri@gmail.com?subject=I want to hire you.' className='link_style' style={{ textDecoration: 'none' }}><p>CONTACT US</p></a>
+                            <a href='https://github.com/legsateri' className='link_style' target='_blank' style={{ textDecoration: 'none' }}><p>GITHUB</p></a>
+                            <a href='https://allegrapusateri.com/' className='link_style' target='_blank' style={{ textDecoration: 'none' }}><p className='hide'>PORTFOLIO</p></a>
+                        </div>
+
+                        <div className='footer_container'>
+                            <div className='footer'></div>
+                        </div>
+                    </div>
                 </footer>
+
             </>
         )
     }
