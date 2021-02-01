2a1e191cf9fda15add08f0358fdc9bca292b1b1d
Allegra Pusateri <allegrapusateri@gmail.com>
2021-01-31 20:47:17 -0600

    Set up Barrier Page, minus images and PDF for button link

--- src/routes/BarrierPage/BarrierPage.js
+++ src/routes/BarrierPage/BarrierPage.js
@@ -1,6 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////
 import React, { Component } from 'react';
 //////////////////////////////////////////////////////////////////////////////
+import Barrier from '../VidaPage/vida.png';
+//////////////////////////////////////////////////////////////////////////////
 import './BarrierPage.css';
 //////////////////////////////////////////////////////////////////////////////
 
@@ -9,8 +11,23 @@ class BarrierPage extends Component {
         return (
             <>
                 <main>
-                    <div>
-                        TBD
+                    <div className='barrier_flex'>
+                        <img src={Barrier} alt='Barrier Sample' className='barrier_sample' />
+                        
+                        <div className='center_mobile'>
+                            <h1>Barrier</h1>
+                            
+                            <p>A new diaper rash cream is in town. Barrier is made with natural ingredients by doctors with moms in mind.</p>
+                            <p>We were brought in to create taglines and logos for this new product line. Working with a designer, we did two rounds of exploration—the first being broad and the second narrowing down concepts for the brand owners. The purpose of this exploration was to determine not just how the brand wanted to visually represent themselves going forward, but also to explore how they will talk about their product, which will determine what niche of this market they will be competing within.</p>
+
+                            <h2 className='services'>Services</h2>
+
+                            <ul>
+                                <li>Copywriting</li>
+                            </ul>
+
+                            <a href='https://www.vidasangels.com/home' target='_blank'><button className='download_button'>VIEW CREATIVE</button></a>
+                        </div>
                     </div>
                 </main>
             </>
